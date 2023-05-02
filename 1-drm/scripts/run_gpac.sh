c2957d45bfb094303dc70ebdf9e1c6c9856e280c
juan.adarve <juan.adarve@geniussports.com>
2023-05-02 13:39:08 -0500

    Fix commands for RTMP ingest and DRM.

--- 1-drm/scripts/run_gpac.sh
+++ 1-drm/scripts/run_gpac.sh
@@ -1,3 +1,5 @@
 gpac -i rtmp://:1935/live:gpac:rtmp_listen=1 \
-    cecrypt:cfile="/tmp/workdir/gpac-drm-dash.xml" \
-    -o /tmp/workdir/content/playlist.mpd:segdur=2:cdur=0.2:asto=1.8:cmaf=cmfc:profile=dashif.ll:dmode=dynamic:template=segment_dash:rdirs=gmem:hmode=push:pssh=mv:nosync=false:utcs=inband
+    reframer:rt=on \
+    cecrypt:cfile="/tmp/workdir/gpac-drm-dash.xml":#video:#audio \
+    -o /tmp/workdir/content/playlist.mpd:dmode=dynamic:segdur=1:cdur=0.2:asto=0.8:cmaf=cmfc:profile=dashif.ll:pssh=m:utcs=https://time.akamai.com/?iso \
+    -graph
